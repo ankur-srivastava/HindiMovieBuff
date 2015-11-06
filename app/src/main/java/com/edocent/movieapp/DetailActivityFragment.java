49f71f64c4fe4789ebb619fb6a1479aaa651cc72
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-06 14:05:02 +0530

    Refactor

--- app/src/main/java/com/edocent/movieapp/DetailActivityFragment.java
+++ app/src/main/java/com/edocent/movieapp/DetailActivityFragment.java
@@ -1,11 +1,8 @@
 package com.edocent.movieapp;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,27 +10,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.edocent.movieapp.adapters.TrailerAdapter;
 import com.edocent.movieapp.database.MovieDBHelper;
 import com.edocent.movieapp.model.Movie;
 import com.edocent.movieapp.model.Trailer;
 import com.edocent.movieapp.utilities.AppConstants;
 import com.squareup.picasso.Picasso;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -52,11 +35,7 @@ public class DetailActivityFragment extends Fragment implements AdapterView.OnIt
     TextView movieDetailLength;
     TextView movieDetailRating;
     TextView movieDetailOverview;
-    TextView viewReviewsId;
     TextView trailerView;
-    TrailerAdapter mTrailerAdapter;
-    ListView trailerListView;
-    ReviewScreen mReviewScreen;
 
     ArrayList<Trailer> trailers;
 
@@ -66,16 +45,6 @@ public class DetailActivityFragment extends Fragment implements AdapterView.OnIt
 
     public void setMovieDetailObject(Movie movieDetailObject) {
         this.movieDetailObject = movieDetailObject;
-    }
-
-    static interface ReviewScreen{
-        void displayReviews(long movieId);
-    }
-
-    @Override
-    public void onAttach(Activity activity){
-        super.onAttach(activity);
-        mReviewScreen = (ReviewScreen) activity;
     }
 
     @Override
@@ -205,141 +174,5 @@ public class DetailActivityFragment extends Fragment implements AdapterView.OnIt
                 Toast.makeText(getActivity(), "Some problem with the video", Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    public class VideoURLService extends AsyncTask<Long, Void, String> {
-
-        private ProgressDialog dialog =
-                new ProgressDialog(getActivity());
-
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
-            dialog.setMessage("Getting the trailers !!");
-            dialog.show();
-        }
-
-        @Override
-        protected String doInBackground(Long... params) {
-            long movieId = 0;
-
-            if(params[0] != null){
-                movieId = params[0];
-                if(movieId != 0){
-                    return getTrailersJSONString(movieId);
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result){
-            if(result != null && !result.equals("")){
-                JSONObject jsonObject = null;
-                JSONArray jsonArray = null;
-                trailers = new ArrayList<>();
-
-                try {
-                    jsonObject = new JSONObject(result);
-                } catch (JSONException e) {
-                    Log.e(TAG, e.getMessage());
-                }
-
-                if(jsonObject != null){
-                    try {
-                        jsonArray = jsonObject.getJSONArray("results");
-                    } catch (JSONException e) {
-                        Log.e(TAG, e.getMessage());
-                    }
-                }
-
-                if(jsonArray != null && jsonArray.length() > 0){
-                    for(int i=0; i<jsonArray.length(); i++){
-                        JSONObject tempJsonObject = null;
-                        try {
-                            tempJsonObject = jsonArray.getJSONObject(i);
-                        } catch (JSONException e) {
-                            Log.e(TAG, e.getMessage());
-                        }
-                        if(tempJsonObject != null){
-                            Trailer trailer = new Trailer();
-
-                            try {
-                                trailer.setTrailerKey(tempJsonObject.getString("key"));
-                                trailer.setTrailerName(tempJsonObject.getString("name"));
-                            } catch (JSONException e) {
-                                Log.e(TAG, e.getMessage());
-                            }
-                            trailers.add(trailer);
-                        }
-                    }
-                }
-                if(trailers != null && trailers.size() > 0){
-                    Log.v(TAG, "Trailers List "+trailers.size());
-                    movieDetailObject.setTrailersList(trailers);
-                    mTrailerAdapter = new TrailerAdapter(getActivity(), R.layout.list_item_trailer, trailers);
-                    trailerListView.setAdapter(mTrailerAdapter);
-                }
-                dialog.dismiss();
-            }
-        }
-    }
-
-    /*
-    Get data from the service
-    This code has been borrowed from https://gist.github.com/udacityandroid/d6a7bb21904046a91695
-    * */
-    public String getTrailersJSONString(long movieId){
-        HttpURLConnection urlConnection = null;
-        BufferedReader reader = null;
-        String movieTrailerJsonStr = null;
-
-        try {
-            String base_url = AppConstants.TRAILERS_BASE_URL+"/"+movieId+"/videos";
-            Uri uri= Uri.parse(base_url).buildUpon()
-                    .appendQueryParameter(AppConstants.API_KEY, AppConstants.MOVIE_API_KEY)
-                    .build();
-
-            Log.v(TAG, "Trailers URI - "+uri.toString());
-            URL url = new URL(uri.toString());
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            InputStream inputStream = urlConnection.getInputStream();
-            if (inputStream == null) {
-                return null;
-            }
-            reader = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            StringBuffer buffer = new StringBuffer();
-
-            while ((line = reader.readLine()) != null) {
-                buffer.append(line + "\n");
-            }
-
-            if (buffer.length() == 0) {
-                return null;
-            }
-            movieTrailerJsonStr = buffer.toString();
-        } catch (IOException e) {
-            Log.e(TAG, "Error ", e);
-            return null;
-        } finally{
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (final IOException e) {
-                    Log.e(TAG, "Error closing stream", e);
-                }
-            }
-        }
-
-        return movieTrailerJsonStr;
     }
 }
