e08d6ab0100c97931e204b06d54f3ca654d88ef1
Ankur Srivastava <ankurlkw@gmail.com>
2015-11-06 11:08:37 +0530

    Reformat

--- app/src/main/java/com/edocent/movieapp/utilities/AppConstants.java
+++ app/src/main/java/com/edocent/movieapp/utilities/AppConstants.java
@@ -1,42 +1,24 @@
 package com.edocent.movieapp.utilities;
 
 /**
- * Created by SRIVASTAVAA on 10/20/2015.
+ * Created by Ankur on 10/20/2015.
  */
 public class AppConstants {
-    //Please modify this key when using this App : https://www.themoviedb.org/
-    //public static final String MOVIE_API_KEY = "2488d2824d22372dac5e1c8f6e779c5f";
     public static final String MOVIE_API_KEY = "6CC981035004FFA3DFD8D2555C348502";
-    //public static final String BASE_URL = "http://api.themoviedb.org/3/discover/movie";
     public static final String BASE_URL = "http://api.cinemalytics.com/v1/analytics/TopMovies";
-    public static final String SORT_BY="sort_by";
-    public static final String PAGE_NO="page";
-    public static final String RELEASED_THIS_WEEK="http://api.cinemalytics.com/v1/movie/releasedthisweek";
     public static final String NEXT_RELEASE_URL="http://api.cinemalytics.com/v1/movie/nextchange";
-    //public static final String API_KEY="api_key";
     public static final String API_KEY="auth_token";
-    public static final String POPULARITY = "popularity.desc";
-    public static final String RATING = "vote_average.desc";
-    public static final String POPULAR_MOVIES_URL = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="+MOVIE_API_KEY;
-    public static final String MOVIE_URL="http://image.tmdb.org/t/p/w342";
-    public static final String MOVIE_DETAIL_URL="http://image.tmdb.org/t/p/w780";
     public static final String DETAIL_MOVIE_OBJECT="movie_detail";
     public static final String MOVIE_LIST_FROM_BUNDLE_KEY="movie_list_from_bundle_key";
     public static final String NEXT_MOVIE_LIST_FROM_BUNDLE_KEY="next_movie_list_from_bundle_key";
     public static final String MOVIE_DTL_FROM_BUNDLE_KEY="movie_list_from_bundle_key";
-    public static final int FETCH_LIMIT=12;
     public static final String TRAILERS_BASE_URL = "http://api.themoviedb.org/3/movie";
-    public static final String MOVIE_YOUTUBE_URL="https://youtu.be";
-    // http://api.themoviedb.org/3/movie/177677/reviews?api_key=2488d2824d22372dac5e1c8f6e779c5f
     public static final String MOVIE_REVIEWS_URL="http://api.themoviedb.org/3/movie";
     public static final int REVIEW_TITLE_LENGTH=80;
     public static final String DB_NAME="Movie";
     public static final int DB_VERSION=1;
     public static final String FAVORITE_MOVIE="Y";
     public static final String NOT_FAVORITE_MOVIE="N";
-
-    //DB Constants
     public static final String MOVIE_TITLE="MOVIETITLE";
-
     public static final int PROGRESS_DIALOG_TIME=6000;
 }
